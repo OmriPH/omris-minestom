67b25d8fe9ee54670ff0d1355d4d1a0f41afaa62
Omri <omri.hermon@gmail.com>
2024-10-18 16:24:43 +0300

    Simple Minestom server with Flat World

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("java")
 }
 
-group = "org.example"
+group = "me.omrih"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -12,6 +12,8 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("net.minestom:minestom-snapshots:0ca1dda2fe")
+    implementation("org.slf4j:slf4j-simple:2.0.16")
 }
 
 tasks.test {
--- src/main/java/me/omrih/Main.java
+++ src/main/java/me/omrih/Main.java
@@ -1,7 +1,43 @@
 package me.omrih;
 
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.entity.Player;
+import net.minestom.server.event.GlobalEventHandler;
+import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
+import net.minestom.server.extras.MojangAuth;
+import net.minestom.server.instance.InstanceContainer;
+import net.minestom.server.instance.InstanceManager;
+import net.minestom.server.instance.LightingChunk;
+import net.minestom.server.instance.block.Block;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Initialize the server
+        MinecraftServer server = MinecraftServer.init();
+
+        // Create Instance
+        InstanceManager instanceManager = MinecraftServer.getInstanceManager();
+        InstanceContainer instanceContainer = instanceManager.createInstanceContainer();
+
+        // generate world
+        instanceContainer.setGenerator(unit -> unit.modifier().fillHeight(0, 40, Block.GRASS_BLOCK));
+
+        // add lighting
+        instanceContainer.setChunkSupplier(LightingChunk::new);
+
+        // Add event handler to handle player spawning
+        GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
+        globalEventHandler.addListener(AsyncPlayerConfigurationEvent.class, event -> {
+            final Player player = event.getPlayer();
+            event.setSpawningInstance(instanceContainer);
+            player.setRespawnPoint(new Pos(0, 42, 0));
+        });
+
+        // enable online mode
+        MojangAuth.init();
+
+        // Start the server
+        server.start("0.0.0.0", 62309);
     }
 }
